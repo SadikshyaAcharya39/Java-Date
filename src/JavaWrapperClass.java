1cbb5239c9386a20ad5ce0fece69afea4db74569
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-01 23:45:23 +0545

    First Commit

---
+++ src/JavaWrapperClass.java
@@ -0,0 +1,11 @@
+public class JavaWrapperClass {
+    public static void main(String[] args) {
+
+        Double myDouble = 20.0;
+        Integer myInt = 3;
+        Character myChar = 'A';
+        System.out.println(myDouble);
+        System.out.println(myInt);
+        System.out.println(myChar);
+    }
+}
