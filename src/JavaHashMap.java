ebf81d25d995f091acf44a1889add1115ddb8c23
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-01 23:35:13 +0545

    first commit

---
+++ src/JavaHashMap.java
@@ -0,0 +1,51 @@
+
+import java.util.HashMap;
+
+public class JavaHashMap {
+
+    public static void main(String[] args) {
+
+//        HashMap<String, String> capitalCities = new HashMap<String, String>();
+//
+//
+//        // Add keys and values ( Country, City)
+//
+//        capitalCities.put(" Nepal ", " Kathmandu");
+//        capitalCities.put(" India ", " New Delhi");
+//        System.out.println(capitalCities);
+
+        HashMap<String, Integer> numbers = new HashMap<>();
+        numbers.put("num1", 100);
+        numbers.put("num2", 200);
+//        System.out.println(numbers);
+
+//        System.out.println(numbers.get("num1"));
+
+        // Remove an item
+//        numbers.remove("num1");
+//      System.out.println(numbers);
+
+//        numbers.clear();
+//        System.out.println(numbers);
+//
+//        // Size and Length
+//        System.out.println(numbers.size());
+
+
+        // Loop through a HashMap
+
+//        for(String i: numbers.keySet()){
+//            System.out.println(i);
+//    }
+
+//        for(int i: numbers.values()){
+//            System.out.println(i);
+//        }
+
+//        for(String i: numbers.keySet()){
+//            System.out.println("Key : " + i + "   " +  "Values : " + numbers.get(i));
+//        }
+
+
+    }
+}
