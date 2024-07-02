43681eb95e01be4b67032f1243a5d755166cd7bb
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-02 23:22:09 +0545

    First Commit

---
+++ src/Mains.java
@@ -0,0 +1,15 @@
+
+public class Mains {
+    enum Anno {
+        LOW,
+        MEDIUM,
+        HIGH;
+
+    }
+
+ public static void main(String[] args) {
+    Anno anno = Anno.MEDIUM;
+     System.out.println(anno);
+
+ }
+ }   
