8d840314764b7824f4c482b563a1fb4b6105eded
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-04 00:18:19 +0545

    First Commit

---
+++ src/NestedIfElse.java
@@ -0,0 +1,46 @@
+public class NestedIfElse {
+    public static void main(String[] args) {
+//        if (true) {
+//            if (true) {
+//
+//                System.out.println("abc");
+//
+//            } else {
+//                System.out.println("xyz");
+//            }
+//        } else {
+//            System.out.println("123");
+//        }
+
+
+//        if (false) {
+//            if (true) {
+//
+//                System.out.println("abc");
+//
+//            } else {
+//                System.out.println("xyz");
+//            }
+//        } else {
+//            System.out.println("123");
+//        }
+
+//        if (true) {
+//            if (false) {
+//
+//                System.out.println("abc");
+//
+//            } else {
+//                System.out.println("xyz");
+//            }
+//        } else {
+//            System.out.println("123");
+//        }
+
+        int a = 20;
+        int b = 30;
+        int x = (a > b)? a:b;
+
+
+    }
+}
