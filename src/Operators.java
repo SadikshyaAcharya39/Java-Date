8f6b9df2790b7e850d0607748f701409ad120a62
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-04 00:17:11 +0545

    First Commit

---
+++ src/Operators.java
@@ -0,0 +1,8 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+        System.out.println(a > b);
+        System.out.println(a >= b);
+    }
+}
