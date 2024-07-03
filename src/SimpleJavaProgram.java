fc13d66780ebca7608623a7b573039f49d8d857b
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-04 00:16:09 +0545

    First Commit

---
+++ src/SimpleJavaProgram.java
@@ -0,0 +1,40 @@
+public class SimpleJavaProgram {
+    public static void main(String[] args) {
+
+//        int a = 100;
+//        System.out.println(a);
+//
+//        a = 200;
+//        System.out.println(a);
+
+//        int a, b, c;
+//        a = 200;
+//        b = 300;
+//        c = 250;
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+
+
+        int a = 100, b = 50, c = 250;
+        byte x = 127;
+
+
+        // Data Type:
+
+        /*
+
+       1. Primitive Data Type:
+
+       Byte, Short, Int, Long --> Number without decimal
+       Float, Double --> Decimal Number
+
+       Float, Double, Int, Short, Byte, Short, Boolean, Long, Char
+
+       2. Non - Primitive Data Type: Derived Data Types, Collections
+       Class, Objects, Arrays, String,
+       So many collection Classes: ArrayList, HashMap, HashSet
+
+         */
+    }
+}
