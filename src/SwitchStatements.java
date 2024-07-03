78eb95f6cf1ad65a34bc3d3db71825f708caea23
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-04 00:14:29 +0545

    First Commit

---
+++ src/SwitchStatements.java
@@ -0,0 +1,20 @@
+public class SwitchStatements {
+    public static void main(String[] args) {
+        String days = "Friday";
+        switch(days){
+            case "Sunday":
+                System.out.println(1);
+            case "Monday":
+                System.out.println(2);
+            case "Tuesday":
+                System.out.println(3);
+            case "Wednesday":
+                System.out.println(4);
+            case "Thursday":
+                System.out.println(5);
+            case "Friday":
+                System.out.println(6);
+
+        }
+    }
+}
