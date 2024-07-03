16f052280b76cd87251395f9307e47688daf28d1
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-04 00:19:44 +0545

    First Commit

---
+++ src/MultipleStatements.java
@@ -0,0 +1,28 @@
+public class MultipleStatements {
+    public static void main(String[] args) {
+
+//        if(true){
+//
+//            System.out.println(1);
+//        }
+//        else{
+//            System.out.println(2);
+//        }
+
+
+//        if(1 == 1){
+//            System.out.println(1);
+//        }
+//        else{
+//            System.out.println(2);
+//        }
+
+        if(1 == 2){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(2);
+        }
+
+    }
+}
