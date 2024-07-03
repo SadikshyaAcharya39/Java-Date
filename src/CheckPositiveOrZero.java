a00437a37ae8aefdcd948923d82bd9fae9e33716
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-04 00:25:45 +0545

    First Commit

---
+++ src/CheckPositiveOrZero.java
@@ -0,0 +1,14 @@
+public class CheckPositiveOrZero {
+    public static void main(String[] args) {
+       int num = -10;
+       if(num == 0){
+           System.out.println("Equal To Zero...");
+       }
+       else if(num < 0){
+           System.out.println("Number is a negative number...");
+       }
+       else{
+           System.out.println(" Is a positive number");
+       }
+    }
+}
