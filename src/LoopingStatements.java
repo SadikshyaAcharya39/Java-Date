de2c4f21a87dd4ca62f756123564da0f485e6d0d
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-04 22:41:15 +0545

    First Commit

---
+++ src/LoopingStatements.java
@@ -0,0 +1,130 @@
+public class LoopingStatements {
+    public static void main(String[] args) {
+        /*
+        1. While Loop
+        2. Do-While Loop
+        3. For Loop
+        4. For-each Loop
+        4. Nested Loops
+
+         */
+
+//        int a = 1;
+//        while(a <= 10){
+//            System.out.println(a);
+//            a++;
+//        }
+
+//        do{
+//            System.out.println(a);
+//            a++;
+//        }while (a <= 10);
+
+//        int num = 3;
+//        for(int a = 1; a<= 10; a++){
+//            System.out.println(num + " * " + a + " = " + num * a);
+//        }
+
+
+
+
+        // Print the table of 3 using while loop
+
+//        int num = 3;
+//        int a = 1;
+//        while( a <= 10){
+//            System.out.println(num + " * " + a + " = " + num * a);
+//            a++;
+//        }
+
+
+//        int num = 3;
+//        int a = 1;
+//        do{
+//            System.out.println(num + " * " + a + " = " + num * a);
+//            a++;
+//        }
+//        while(a <= 10);
+
+        // Print 1 to 10 using while loop
+
+//        int a = 2;
+//        while( a <= 10){
+//            System.out.println(a);
+//            a += 2;
+//        }
+
+        // For odd numbers
+//        int a = 1;
+//        while( a <= 10){
+//            System.out.println(a);
+//            a += 2;
+//        }
+
+
+        // To find even numbers
+
+//        int i = 1;
+//        while(i <= 10) {
+//            if (i % 2 == 0) {
+//                System.out.println( "Even Number: " + i );
+//            }
+//            else{
+//                System.out.println("Odd Number: " + i);
+//            }
+//            i++;
+//        }
+
+        // Print 10 to 1
+
+//        int a = 10;
+//        while( a > 0){
+//            System.out.println(a);
+//            a--;
+//        }
+
+
+//        int a = 10;
+//        do{
+//            System.out.println(a);
+//            a--;
+//        }
+//        while(a >= 1); // a > 0
+
+//        int a = 1;
+//        do{
+//            System.out.println(a);
+//            a++;
+//        }while(a <= 5);
+
+
+
+//        for(int a = 2; a <=10; a+=2){
+//            System.out.println(a);
+//        }
+
+//        for(int a = 1; a <=10; a+=2){
+//            System.out.println(a);
+//        }
+
+        // For Loop with conditional statements
+        // To print even numbers
+
+//        for(int i= 1; i <= 10; i++){
+//            if( i % 2 == 0){
+//                System.out.println("Even numbers: " + i);
+//            }
+//            else{
+//                System.out.println("Odd Numbers: " + i);
+//            }
+//        }
+
+
+//        for(int i = 10; i > 0; i--){
+//            System.out.println(i);
+//        }
+
+
+
+   }
+}
