1601d95b6e8d6d35b714ddc7e7481ff3544877ee
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-04 22:45:43 +0545

    First Commit

---
+++ src/WhileLoopWithOutCondition.java
@@ -0,0 +1,18 @@
+public class WhileLoopWithOutCondition {
+    public static void main(String[] args) {
+        int i = 1;
+//        while(true){
+//            System.out.println("Hello");
+//            i++;
+//
+//            if( i == 10){
+//                break;
+//            }
+//            // Based on certain condition we want to break the loop
+//        }
+
+      //  if condition is true then only these condition will be executed.
+
+
+    }
+}
