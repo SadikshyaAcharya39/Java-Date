a91f714205ab1cd91fd3af6145f67f324e8c0787
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-09 21:49:38 +0545

    First Commit

---
+++ src/OperationsString.java
@@ -0,0 +1,23 @@
+public class OperationsString {
+    public static void main(String[] args) {
+
+        // Using String Buffer/ String Builder
+
+//        StringBuilder sb = new StringBuilder("Hello");
+//        sb.append(" World");
+//        System.out.println(sb);
+
+
+//        StringBuffer sb = new StringBuffer("Sadikshya");
+//        sb.append(" Acharya");
+//        System.out.println(sb);
+
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(122);
+        System.out.println(stringBuffer);
+
+
+
+
+    }
+}
