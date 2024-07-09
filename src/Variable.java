97b40c0fcf1a574e91d4178b7551ea0c0de624d0
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-09 21:47:47 +0545

    First Commit

--- src/Variable.java
+++ src/Variable.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Variable {
     public static void main(String[] s) {
 
@@ -111,10 +113,126 @@ public class Variable {
 
         // Calculate the Area Of Rectangle
 
-        float length = 20f;
-        float width = 10f;
-        float areaOfRectangle = length * width;
-        System.out.println(areaOfRectangle);
+//        float length = 20f;
+//        float width = 10f;
+//        float areaOfRectangle = length * width;
+//        System.out.println(areaOfRectangle);
+
+//        float length = 20f;
+//        float width = 10f;
+//        float areaOfRectangle;
+//        areaOfRectangle = length * width;
+//        System.out.println(areaOfRectangle);
+
+//        float f1 = 35e3f;
+//        System.out.println(f1);
+
+//        float f1 = 34E3f;
+//        System.out.println(f1);
+
+
+//        boolean isJavaFun = true;
+//        boolean isJavaFishy = false;
+//        System.out.println(isJavaFun);
+//        System.out.println(isJavaFishy);
+
+//        char myVar1 = 12;
+//        System.out.println(myVar1);
+
+//        String name = "   ";
+//        System.out.println("Name: " + name);
+
+
+        // Java Type Casting
+
+        // Two types of Casting:
+
+        /*
+
+        Widening Casting: byte - short - char - int - long - float - double
+
+        Narrowing Casting: double - float - long - int - char - short - byte
+
+         */
+
+        // Widening Casting
+
+//        int myInt = 9;
+//        double myDouble = myInt;
+//        System.out.println(myInt);
+//        System.out.println(myDouble);
+
+        // Narrowing Casting
+
+//        double myDouble = 9.78d;
+//        int myInt = (int) myDouble;
+//        System.out.println(myInt);
+//        System.out.println(myDouble);
+
+
+        // Real-Life Example
+
+//        int userScore = 390;
+//        int maxScore = 500;
+//        float percentage = (float) userScore / maxScore * 100.0f;
+//        System.out.println(percentage);
+
+//        int x = 1;
+//        System.out.println(x ^ 11);
+
+
+//        String name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//        System.out.println(name.length());
+
+//        String name = "     Sadikshya           ";
+//          String name = " Sadikshya";
+//        System.out.println(name.charAt(0));
+//        System.out.println(name.toUpperCase());
+//        System.out.println(name.toLowerCase());
+//        System.out.println(name.trim());
+//        System.out.println(name.substring(3));
+//        System.out.println(name.indexOf(1));
+//        System.out.println(name.equals(" Sadikshya"))
+
+//        SPACE chai hunu hudeina alphabet har chai upperCase lowerCase bhayera kei hudeina.
+//        System.out.println(name.equalsIgnoreCase(" sadiKsHya"));
+//        System.out.println(name.equalsIgnoreCase("sadiKsHya"));
+
+//        System.out.println(name.indexOf("S"));
+
+
+//        String name = "  Sadikshya" + " " +  "Acharya";
+//        System.out.println(name.trim());
+
+
+//        String name = "Sadikshya";
+//        boolean isEqual = "Sadikshya" .equals("Sadikshya");
+//        System.out.println(isEqual);
+
+//        System.out.println(name.replace("Sadikshya" , "Acharya"));
+//        System.out.println(name.replace('S', 's'));
+
+
+//        String name = "Sadikshya Acharya ";
+//        String[] splitName = name.split(" ");
+//        System.out.println(Arrays.toString(splitName));
+
+//        String name = "Sadikshya Acharya";
+//        String[] splitName = name.split(" ");
+//        System.out.println(Arrays.toString(splitName));
+
+
+//        String name = "Sadikshya Acharya";
+//        String[] splitName = name.split(" ");
+//        System.out.println(Arrays.toString(splitName));
+
+//        String name = "Sadikshya Acharya";
+//        String[] splitName = name.split(" ");
+//
+//        for(String i: splitName){
+//            System.out.println(i);
+//        }
+
 
 
 
