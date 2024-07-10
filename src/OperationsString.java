04f3a88b490d381b19b79325441ccb12a73b30c7
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-10 23:14:02 +0545

    First Commit

--- src/OperationsString.java
+++ src/OperationsString.java
@@ -1,3 +1,7 @@
+import jdk.jfr.Percentage;
+
+import java.util.Arrays;
+
 public class OperationsString {
     public static void main(String[] args) {
 
@@ -12,9 +16,102 @@ public class OperationsString {
 //        sb.append(" Acharya");
 //        System.out.println(sb);
 
-        StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append(122);
-        System.out.println(stringBuffer);
+//        StringBuffer stringBuffer = new StringBuffer();
+//        stringBuffer.append(122);
+//        System.out.println(stringBuffer);
+
+//        String name = "Amir";
+//        System.out.println(name.concat(" Acharya"));
+
+//        String name = "An";
+//        String  name2 = "ju";
+//        String fullName = name + name2;
+//        System.out.println(fullName);
+
+//        String name = "Sadikshya";
+//        System.out.println(name.equals("sadikshya"));
+
+//        String name1 = "Sadikshya";
+//        String name2 = "sadikshya";
+//        boolean combined = name1.equals(name2);
+//        System.out.println(combined);
+
+//         String name = "Sadikshya";
+//         boolean combined = name.contains("Acharya");
+//         System.out.println(combined);
+
+//        String name = "Sadikshya";
+//        boolean contains = name.contains("Sadikshya");
+//        System.out.println(contains);
+
+//        String str = "Hello, World!";
+//        System.out.println(str.replace("World", "Java"));
+
+
+
+//        String name = " Sadikshya ";
+//        System.out.println(name.trim());
+
+//         String name = "Sadikshya Acharya";
+//         String[] splitName = name.split(" , " );
+//         for(String i: splitName)
+//             System.out.println(i);
+
+//         System.out.println(name.split(" , "));
+
+
+//         String firstName = "John";
+//         String lastName = " Doe";
+//         System.out.println(firstName.concat(  lastName));
+
+
+//        String name = "Nepal is a wonderful country";
+//        System.out.println("\"Nepal\" is a wonderful country");
+
+
+//          String name = "Sadikshya";
+//        System.out.println(name.charAt(0));
+//        System.out.println(name.indexOf('S'));
+
+        // String Splitting
+
+//        String fruits = "apple,banana,orange";
+//        String[] fruitsName = fruits.split(" , ");
+//        System.out.println(Arrays.toString(fruitsName));
+
+
+        // Formatting String
+
+//        String name = "John";
+//        int age = 30;
+//        float percentage  = 90f;
+//        String formatted = String.format("Name:  %s, Age: %d , percentage: %f ", name , age, percentage);
+//        System.out.println(formatted);
+
+//        String name = "Sadikshya Acharya";
+//        int age = 20;
+//        float percentage = 86f;
+//        double overall = 100;
+//        System.out.println(String.format("Name: %s Age: %d Percentage: %.2f Overall: %.2f", name, age, percentage, overall));
+
+//        String str = "Hello World!";
+//        boolean startsWith = str.startsWith("Hello");
+//        boolean endsWith = str.endsWith("!");
+//        System.out.println(startsWith);
+//        System.out.println(endsWith);
+
+        // Regular Expressions
+
+//        String str = "abc123xyz";
+//        boolean matches = str.matches("[a-z] + \\d + d+[a-z]");
+//        String replaced = str.replace("\\d+", "456");
+//        System.out.println(matches);
+//        System.out.println(replaced);
+
+//        String something = "abc123xss";
+//        boolean matches = something.matches("[a-z]+\\d+[a-z]+");
+//        System.out.println(matches);
+
 
 
 
