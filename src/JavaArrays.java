4651607e492be9be2ecf0dfaa58d3afdbbb916a0
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-11 20:07:28 +0545

    First Commit

---
+++ src/JavaArrays.java
@@ -0,0 +1,89 @@
+public class JavaArrays {
+    public static void main(String[] args) {
+//        String[] name = {"Sadikshya", "Amshu", "Amir", "Anju"};
+
+//        for(String i: name){
+//            System.out.println(i);
+//        }
+
+//        for(int i = 0; i <= name.length-1; i++){
+//            System.out.println(name[i]);
+//        }
+
+//        for(int i = name.length- 1; i >= 0; i--){
+//            System.out.println(name[i]);
+//        }
+
+//        System.out.println(name);
+
+
+        // String reverse
+
+//        String name = "Sadikshya";
+//        for(int i = name.length() - 1; i >= 0; i--){
+//            System.out.print(name.charAt(i));
+//        }
+
+        // Factorial
+
+//        int num = 5;
+//        int fact = 1;
+//        for(int i = 1; i <= num; i++){
+//            fact *= i;
+//        }
+//        System.out.println(fact);
+
+//        int num = 5;
+//        int fact = 1;
+//        for(int i = 1; i <= num; i++){
+//            fact = fact * i;
+//            System.out.println(fact);
+//        }
+
+
+//         int num = 3;
+//         int fact = 1;
+//         for(int i = 1; i <= num; i++){
+//             fact = fact * i;
+//         }
+//        System.out.println(fact);
+
+
+//          String countryName = "Nepal";
+//          for(int i = countryName.length() - 1; i >= 0; i--){
+//              System.out.print(countryName.charAt(i));
+//          }
+
+//           int[] num = {2, 4, 6, 8, 10};
+//        for(int i = 0; i < num.length; i++){
+//            System.out.println(num[i]);
+//        }
+
+
+//        Integer age = 20;
+//        System.out.println(age);
+
+
+//        int[] num = {10, 20, 30, 40, 50};
+//        for(int i = 0; i < num.length; i++){
+//            System.out.println(num[i]);
+//        }
+
+
+//        int[] num = {10, 20, 30, 40, 50};
+//        for(int i: num){
+//            System.out.println(i);
+//        }
+
+//       int[][] nums = {{10, 20, 30, 40, 50}, {1, 2, 3, 4, 5}};
+//       for(int i = 0; i < nums.length; i++){
+//           for(int j = 0; j < nums[i].length; j++){
+//               System.out.println(nums[i][j]);
+//           }
+//       }
+
+
+
+
+    }
+}
