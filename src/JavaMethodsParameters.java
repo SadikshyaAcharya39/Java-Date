94874b89be9f50d09ca670066ea6ecb183dd012e
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-11 22:12:52 +0545

    First Commit

---
+++ src/JavaMethodsParameters.java
@@ -0,0 +1,10 @@
+public class JavaMethodsParameters {
+
+    static void myMethod(String fname){
+        System.out.println(fname + " Acharya");
+    }
+
+    public static void main(String[] args) {
+        myMethod("Sadikshya");
+    }
+}
