c8411c9a767241eb5152f83189333eae9cf7b3d9
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-11 18:08:10 +0545

    First Commit

---
+++ src/JavaMaths.java
@@ -0,0 +1,46 @@
+public class JavaMaths {
+    public static void main(String[] args) {
+
+//        int x = 20;
+//        int y = 50;
+//        System.out.println(Math.max(x,y));
+//        System.out.println(Math.min(x, y));
+
+//          System.out.println(Math.random() * 101);
+
+//        System.out.println(Math.sqrt(64));
+
+          // Absolute Value
+
+//        System.out.println(Math.abs(-10));
+
+
+//        System.out.println((int) (Math.random() * 101));
+
+//        System.out.println(Math.max(200, 500));
+//        System.out.println();
+
+//          int x = 10;
+//          int y = 9;
+//        System.out.println(x > y);
+
+//          int x = 10;
+//        System.out.println(x == 10);
+
+//          if(20 > 18){
+//              System.out.println("20 is greater than 18..");
+//    }
+
+//        int x = 18;
+//        int y = 20;
+//        if(x < y){
+//            System.out.println("X is greater than Y");
+//        }
+
+//         int time = 20;
+//         if(time <= 24){
+//             System.out.println("");
+//         }
+
+    }
+}
