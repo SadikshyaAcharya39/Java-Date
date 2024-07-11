1fb3d81acce12986354e9efc2b44e411f2410325
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-11 22:57:44 +0545

    First Commit

---
+++ src/ObjectOrientedProgramming.java
@@ -0,0 +1,27 @@
+
+public class ObjectOrientedProgramming {
+
+
+    // Class - Fruit    Objects - Apple, Banana, Mango
+
+//    int x = 10;
+//
+//    public static void main(String[] args) {
+//        ObjectOrientedProgramming oop = new ObjectOrientedProgramming();
+//        System.out.println(oop.x);
+//    }
+
+
+//    int x = 3;
+//    public static void main(String[] args) {
+//    ObjectOrientedProgramming oop1 = new ObjectOrientedProgramming();
+//    ObjectOrientedProgramming oop2 = new ObjectOrientedProgramming();
+//    System.out.println(oop1.x);
+//    System.out.println(oop2.x);
+//  }
+
+
+
+
+
+}
