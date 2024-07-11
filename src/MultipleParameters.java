2551e8167aa55922a64b67c109dbab0884aa53a0
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-11 22:16:40 +0545

    First Commit

---
+++ src/MultipleParameters.java
@@ -0,0 +1,65 @@
+public class MultipleParameters {
+
+
+//    static void myMethod(String fname, int age){
+//        System.out.println(fname + " is " + age);
+//    }
+//
+//    public static void main(String[] args) {
+//        myMethod("Sadikshya", 20);
+//    }
+
+//    static void checkAge(int age){
+//        if( age > 20) {
+//            System.out.println("Age is greater than 20.");
+//        }
+//        else if (age == 20){
+//            System.out.println("Age is equal to 20.");
+//        }
+//        else{
+//            System.out.println("Age is less than 20.");
+//        }
+//    }
+
+
+
+//    public static void main(String[] args) {
+//        checkAge(20);
+//    }
+
+//    static int addNumbers(int a, int b){
+//        return a + b ;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(addNumbers(2,3));
+//    }
+
+//    static int subtractNumbers(int x, int y){
+//        return x + y;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(subtractNumbers(2, 3));
+//    }
+
+
+//    static void myMethod(int a, int b){
+//        System.out.println(a + b);
+//    }
+//
+//    public static void main(String[] args) {
+//        myMethod(2, 4);
+//    }
+
+//    static int addNumbers(int a, int b){
+//        return a + b;
+//    }
+//
+//    public static void main(String[] args) {
+//        int z = addNumbers(5, 5);
+//        System.out.println("Sum of numbers: " + z);
+//    }
+
+
+}
