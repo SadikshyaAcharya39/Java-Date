0c2a2190b9454c9a72b00deac533e4cb90e5a234
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-11 22:21:00 +0545

    First Commit

---
+++ src/MethodScope.java
@@ -0,0 +1,14 @@
+public class MethodScope {
+    String name = "Sadikshya Acharya";
+
+    public static void main(String[] args) {
+        MethodScope methodScope = new MethodScope();
+        String printName = methodScope.name;
+        System.out.println(printName);
+
+
+//        System.out.println(methodScope.name);
+
+
+    }
+}
