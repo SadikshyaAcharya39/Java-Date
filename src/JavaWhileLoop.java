811c47ef18fd6e1285e9cc9fd246ee3ad9542b61
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-11 18:05:42 +0545

    First Commit

---
+++ src/JavaWhileLoop.java
@@ -0,0 +1,48 @@
+public class JavaWhileLoop {
+    public static void main(String[] args) {
+
+//        int i = 1;
+//        while(i <= 5){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int i = 1;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }
+//        while( i <= 5 );
+//    }
+
+//        int countdown = 3;
+//        do{
+//            System.out.println("Happy Birthday!!!");
+//            countdown --;
+//        }
+//        while( countdown >=1);
+
+//        int countdown = 3;
+//        while (countdown >= 1){
+//            System.out.println(countdown);
+//            countdown --;
+//        }
+//        System.out.println("Happy Birthday!!!");
+
+//        int dice = 1;
+//        while (dice <= 6) {
+//            if (dice < 6) {
+//
+//                System.out.println("No Yatzy!");
+//            } else {
+//                System.out.println("Yatzy!");
+//            }
+//            dice = dice + 1;
+//        }
+
+          for(int i = 2; i <= 20; i +=2){
+              System.out.println(i);
+          }
+
+        }
+    }
