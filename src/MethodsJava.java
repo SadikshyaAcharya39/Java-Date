04089293869a706df65c9a6d9298bed365e8bb1a
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-11 22:18:29 +0545

    First Commit

---
+++ src/MethodsJava.java
@@ -0,0 +1,26 @@
+public class MethodsJava {
+
+
+//    static void printMethodName(){
+//        System.out.println("I just got executed!!!");
+//    }
+//
+//    public static void main(String[] args) {
+//        printMethodName();
+//    }
+
+//    public void printSomething(){
+//        System.out.println("I executed!!!");
+//    }
+//
+//    public static void main(String[] args) {
+//        MethodsJava methodsJava = new MethodsJava();
+//        methodsJava.printSomething();
+//    }
+
+
+
+
+}
+
+
