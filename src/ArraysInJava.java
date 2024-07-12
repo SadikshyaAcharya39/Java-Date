658a9c2d9ca400aa2c1598e82cf82dd19b1aead0
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-12 22:31:17 +0545

    First Commit

---
+++ src/ArraysInJava.java
@@ -0,0 +1,60 @@
+public class ArraysInJava {
+    public static void main(String[] args) {
+
+             // Errors
+        //        String[] names;
+//        names = {"Amir", "Anju"};
+
+
+//        String[] names;
+//        names = new String[3];
+//        names[0] = "Amir";
+//        names[1] = "Sadikshya";
+//        names[2] = "Shraddha";
+////        System.out.println(names);
+//
+//        System.out.println(names.length);
+//
+////        for(String i: names){
+////            System.out.println(i);
+////        }
+//
+//        // Using For Loop
+//
+//        for(int i = 0 ; i < names.length; i++){
+//            System.out.println(names[i]);
+//        }
+
+
+        /*
+        let's create a program that calculates the average of different ages:
+         */
+
+//        int[] ages = {1, 2, 3, 4, 5};
+//
+//        float avg, sum = 0;
+//
+//        // Get the length of the array
+//        int length = ages.length;
+//
+//        for(int i: ages){
+//            sum += i;
+//        }
+//
+//        avg = sum / length;
+//        System.out.println(avg);
+
+
+//        int[] ages = {10, 20, 30, 40, 50, 60, 70, 80, 90, 21, 12, 11, 9 };
+//        int highestAge = ages[0];
+//        for(int i: ages){
+//            if(highestAge < i){
+//                highestAge = i;
+//            }
+//        }
+//
+//        System.out.println(highestAge);
+
+
+    }
+}
