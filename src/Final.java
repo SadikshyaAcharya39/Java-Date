431da62687cd6d98786295dd9a2c6c482dcbde2e
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-13 22:19:34 +0545

    First Commit

---
+++ src/Final.java
@@ -0,0 +1,23 @@
+public class Final {
+
+
+
+//      int x = 10;
+//
+//    public static void main(String[] args) {
+//        Final usingFinal = new Final();
+//        usingFinal.x = 20;
+//        System.out.println(usingFinal.x);
+//        System.out.println("The value of x will be printed if its not final");
+//    }
+
+    final int x = 20;
+
+    public static void main(String[] args) {
+        Final usingFinal = new Final();
+//        usingFinal.x = 20;
+
+        System.out.println("It will generate an error!!!");
+
+    }
+}
