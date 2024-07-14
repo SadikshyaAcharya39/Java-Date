f01a7bc4b64e175b58f7bdff2a5ebec77e48217c
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-14 23:15:27 +0545

    First Commit

---
+++ src/AbstractClass.java
@@ -0,0 +1,14 @@
+abstract class AbstractClass {
+    public String name = "Sadikshya";
+    public int age = 20;
+
+    public abstract void study();
+
+}
+
+// Sub - Class (inherit from Main)
+
+//class Student extends AbstractClass{
+//    public int graduationYear = 2018;
+//
+//}
