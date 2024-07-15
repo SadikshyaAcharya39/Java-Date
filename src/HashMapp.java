3ba7ef0c31f2eef0eae7020b429b6b746406f34f
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-07-15 21:59:41 +0545

    First Commit

---
+++ src/HashMapp.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class HashMapp {
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+        capitalCities.put("Nepal", "Kathmandu");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("USA", "Washington DC");
+        System.out.println(capitalCities);
+    }
+}
